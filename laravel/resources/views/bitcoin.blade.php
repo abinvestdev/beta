cd3b39a3a728d5f5682f5942cfb166cfffa7edf7
Anthony <anthony.invech3e@gmail.com>
2020-10-07 00:03:03 +0800

    bitcoin vaule

--- laravel/resources/views/bitcoin.blade.php
+++ laravel/resources/views/bitcoin.blade.php
@@ -24,33 +24,51 @@
     <section class="content">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-5">
+          <div class="col-6">
             <div class="card">
               <div class="card-header">
-                <h5 class="card-title">Betting Cart</h5>
+                <h5 class="card-title">Betting Cart             <p id="btcValueOld" style="float:right;"></p></h5>
 
               
               </div>
               <!-- /.card-header -->
               <div class="card-body table-responsive p-0" style="height: 300px;">
-              <button class="btn btn-primary btn-sm btn-bet">Start</button>
+              <button class="btn btn-primary btn-sm btn-bet" onClick="startTimer('60',0)">Start</button>
               <button class="btn btn-danger btn-sm btn-bet">Stop</button>
+                      <div class="row">
+
+              <div class="col-6">
+              <h5 style="text-align:right;padding-top:5%;">Minutes</h5>
+              </div>
+              <div class="col-4">
+              <select class="form-control minute_value" >
+             	 <option>1</option>
+             	 <option>2</option>
+             	 <option>3</option>
+             	 <option>4</option>
+              </select>
+              </div>
+              </div>
               <table class="table">
               	<thead>
 	              	<tr>
 	            	  	<th style="width: 1%;text-align:center">Rounds</th>
 	            	  	<th style="width:36%;text-align:center">Amount</th>
-	            	  	<th style="text-align:center">Buy</th>
-	            	  	<th style="text-align:center">Sell</th>
+	            	  	<th style="text-align:center">Up</th>
+	            	  	<th style="text-align:center">Down</th>
+	            	  	<th>Start Value</th>
+	            	  	<th>Time/Status</th>
 	              	</tr>
               	</thead>
               	<tbody>
               	  @for($i=1;$i<13;$i++)
               	 <tr>
               		 <td>{{$i}}</td>
-              		 <td><input type="text" class="form-control" style="text-align:right;color:#fff;"></td>
+              		 <td><input type="text" class="form-control betAmount" style="text-align:right;color:#fff;"></td>
               		 <td style="text-align:center"><input type="radio" name="test_{{$i}}"></td>
               		 <td style="text-align:center"><input type="radio" name="test_{{$i}}"></td>
+              		 <td><p class="oldValue"></p></td>
+              		 <td><div class="time">00:00</div></td>
               	 </tr>
               	 @endfor
 
@@ -60,6 +78,10 @@
               </div>
               
             </div>
+            <div class="col-5">
+            <p id="btcValue" style="display:none;"></p>
+
+            </div>
 
             <!-- /.card -->
           </div>
@@ -67,5 +89,73 @@
         </div>
         </section>
         </div>
+        <script>
+ let oldValue;
+    let socket = new WebSocket("ws://193.108.118.125:3003")
+    socket.onopen = function (evt) {
+        socket.send(JSON.stringify({ 'origin': window.location.hostname, 'source': 'bitmex' }))
+    }
+
+    socket.onmessage = function (evt) {
+        let obj = JSON.parse(evt.data);
+        let classTxt = obj.indicator == '-' ? 'minus' : 'add';
+        document.getElementById('btcValue').innerHTML = `$${obj.value}`
+        document.getElementById('btcValueOld').innerHTML = `$${oldValue}`
+        oldValue = obj.value;
+        document.getElementById('btcValue').classList.remove((classTxt == 'add' ? 'minus' : 'add'))
+        document.getElementById('btcValue').classList.add(classTxt)
+
+        socket.send(JSON.stringify({
+            'continues': true
+        }))
+    }
+
+    socket.onerror = function (e) {
+        console.log(e)
+    }
+    
+    function disconnect() {
+        socket.send(JSON.stringify({disconnect: '!DISCONNECT'}));
+    }
+    function startTimer(duration,display) {
+    	var duration = parseInt(duration) * $('.minute_value').val();
+
+    var timer = duration, minutes, seconds;
+    if($('.betAmount').eq(display).val() != ''){
+    	$('.oldValue').eq(display).text($('#btcValueOld').text());
+
+	    var countDown = setInterval(function startCount() {
+	        minutes = parseInt(timer / 60, 10);
+	        seconds = parseInt(timer % 60, 10);
+
+	        minutes = minutes < 10 ? "0" + minutes : minutes;
+	        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+	        $('.time').eq(display).text(minutes + ":" + seconds);
+
+	        if (--timer < 0) {
+	            timer = duration;
+	        }
+	        if(minutes == 0 && seconds == 0){
+	        	clearInterval(countDown);
+	        	$('.time').eq(display).text($('#btcValueOld').text());
+	        	var next = parseInt(display) + 1;
+	        	startTimer(60,next);
+	        }
+	    }, 1000);
+	}
+	    if($('.betAmount').eq(0).val() == ''){
+	    	alert("please place a bet");
+	    }
+
+
+}
+
+// jQuery(function ($) {
+//     var fiveMinutes = 60 * 1,
+//         display = $('#time');
+//     startTimer(fiveMinutes, display);
+// });
+</script>
 @endsection
 		
