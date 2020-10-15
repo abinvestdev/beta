2997beac8089b0d1ec5cf8db888abc6278c20e60
Anthony <anthony.invech3e@gmail.com>
2020-10-15 17:35:50 +0800

    binance info

--- laravel/resources/views/bitcoin.blade.php
+++ laravel/resources/views/bitcoin.blade.php
@@ -84,7 +84,8 @@
           </div>
               <div class="col-6">
             <p id="btcValue" style="display:none;"></p>
-            <div style="padding-top:200px;height:560px; background-color: #FFFFFF; overflow:hidden; box-sizing: border-box; border: 1px solid #56667F; border-radius: 4px; text-align: right; line-height:14px; font-size: 12px; font-feature-settings: normal; text-size-adjust: 100%; box-shadow: inset 0 -20px 0 0 #56667F;padding:1px;padding: 0px; margin: 0px; width: 100%;"><div style="height:540px; padding:0px; margin:0px; width: 100%;"><iframe src="https://widget.coinlib.io/widget?type=chart&theme=light&coin_id=859&pref_coin_id=1505" width="100%" height="536px" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" style="border:0;margin:0;padding:0;line-height:14px;"></iframe></div><div style="color: #FFFFFF; line-height: 14px; font-weight: 400; font-size: 11px; box-sizing: border-box; padding: 2px 6px; width: 100%; font-family: Verdana, Tahoma, Arial, sans-serif;"><a href="https://coinlib.io" target="_blank" style="font-weight: 500; color: #FFFFFF; text-decoration:none; font-size:11px">Cryptocurrency Prices</a>&nbsp;by Coinlib</div></div>
+            <iframe src="https://bitlive.co.kr/?m=game&m2=binance&m3=i
+    " width="550" height="410" scrolling="no" frameborder="0"></iframe>
 
             </div>
         </div>
@@ -95,7 +96,7 @@
  let oldValue;
     let socket = new WebSocket("ws://193.108.118.125:3003")
     socket.onopen = function (evt) {
-        socket.send(JSON.stringify({ 'origin': window.location.hostname, 'source': 'bitmex' }))
+        socket.send(JSON.stringify({ 'origin': window.location.hostname, 'source': 'binance' }))
     }
 
     socket.onmessage = function (evt) {
